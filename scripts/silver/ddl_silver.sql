acf1972d970ad77b6d112c362aa4009206e5b6dd
SC1601 <108407102+SC1601@users.noreply.github.com>
2026-07-04 18:53:22 +0530

    Add documentation header to ddl_silver.sql
    Added header comment to ddl_silver.sql for documentation.

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -1,1 +1,10 @@
-
+/*
+======================================================================
+DDL Script : Create Silver Tables
+======================================================================
+Script Purpose:
+    This script creates tables in 'silver' schema, dropping tables 
+    if they already exists 
+    Run this script to re-define the DDL structure of 'silver' tables
+======================================================================
+*/
