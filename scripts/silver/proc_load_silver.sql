fde956e22fa46f65c7a07b191a1fd6c11396a47c
SC1601 <108407102+SC1601@users.noreply.github.com>
2026-07-04 18:54:35 +0530

    Document Load Silver Layer stored procedure
    Added documentation for the Load Silver Layer stored procedure, detailing its purpose, actions, parameters, and usage example.

--- scripts/silver/proc_load_silver.sql
+++ scripts/silver/proc_load_silver.sql
@@ -1,1 +1,19 @@
 
+/*
+======================================================================
+Stored Procedure : Load Silver Layer (Bronze -> Silver)
+======================================================================
+Script Purpose:
+    This stored procedure loads data into the 'silver' schema from external csv files.
+    It performs the following actions:
+    - Truncates the silver tables before loading the data
+    - Uses the BULK INSERT' command to load data from csv files to bronze tables.
+
+Parameters:
+    None
+    This stored procedure does not accept any parameters or return any values.
+
+Usage Example:
+    EXEC silver.load_silver;
+======================================================================
+*/
