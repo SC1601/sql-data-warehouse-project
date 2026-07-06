7acdb3f874e9df5975f304fb91b7d631e4e786f0
SC1601 <108407102+SC1601@users.noreply.github.com>
2026-07-06 20:05:24 +0530

    Enhance quality checks in quality_checks_gold.sql
    Updated quality checks to include validation for customer keys.

--- tests/quality_checks_gold.sql
+++ tests/quality_checks_gold.sql
@@ -1,12 +1,26 @@
+/*
+======================================================================
+Quality Checks
+======================================================================
+Script Purpose:
+    This script performs quality checks to validate the integrity, consistency
+    and accuracy of the Gold layer. These checks ensure:
+    - Uniqueness of surrogate keys in dimension tables
+    - Referential integrity between fact and dimension tables.
+    - Validation of relationships in the data model for analytical purposes.
 
-
-
+Usage Notes:
+    - Run these checks after data loading silver and gold layer.
+    - Investigate and resolve any discrepancies found during the checks.
+======================================================================
+*/
 
 --================================================
 -- Create fact table: gold.dim_customers
 --=================================================
 
 --Check data integrity, clean and remove invalid values
+
 SELECT DISTINCT
        ci.cst_gndr,
        ca.gen AS gen_details,
@@ -20,6 +34,17 @@ LEFT JOIN silver.erp_loc_a101 la
 ON         ci.cst_key = la.cid
 
 --================================================
+-- Create fact table: gold.dim_products
+--=================================================
+
+--Check uniqueness of product key
+       
+select product_key, count(*) 
+from gold.dim_products
+group by product_key
+having count(*) > 1
+
+--================================================
 -- Create fact table: gold.fact_sales
 --=================================================
 
@@ -31,4 +56,4 @@ ON         ci.cst_key = la.cid
   ON c.customer_key = f.customer_key
   LEFT JOIN gold.dim_products p
   ON p.product_key = f.product_key
-  where p.product_key IS NULL
+  where p.product_key IS NULL or c.customer_key IS NULL
